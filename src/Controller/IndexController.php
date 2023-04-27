792f429466e948da0594713d86fa0d4562d6f516
Nouster <djebali.mohamed88@gmail.com>
2023-04-27 10:11:37 +0200

    new controller to display all products in home page

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ProductRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class IndexController extends AbstractController
+{
+    #[Route('/', name: 'app_index')]
+    public function index(ProductRepository $productRepository): Response
+    {
+        $products = $productRepository->findAll();
+
+        return $this->render('index/index.html.twig', [
+            'products' => $products,
+        ]);
+    }
+}
